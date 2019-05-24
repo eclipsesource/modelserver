95617f520c37fb4ec74f0e0c95915edb4c9746ea
Tobias Ortmayr <tortmayr@eclipsesource.com>
2019-05-24 11:13:12 +0200

    Initial scaffolding
    - Maven module setup
    - Common libraries as dependencies
    - Inital setup for unit testing
    - License information
    - Travis configuration

---
+++ com.eclipsesource.modelserver.emf/src/main/java/com/eclipsesource/modelserver/emf/DummyEMFModelServer.java
@@ -0,0 +1,12 @@
+package com.eclipsesource.modelserver.emf;
+
+import org.apache.log4j.Logger;
+
+public class DummyEMFModelServer {
+	private static Logger LOG = Logger.getLogger(DummyEMFModelServer.class);
+
+	public static void main(String[] args) {
+		LOG.info("Dummy EMFModelServer has been started");
+	}
+
+}
